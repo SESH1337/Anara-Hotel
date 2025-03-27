96b9af700cd47f798882566b575f0e834c5bb969
Ningendo12 <116316808+Ningendo12@users.noreply.github.com>
2025-03-27 21:34:25 +0400

    Initial commit

---
+++ anara-hotel/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AboutPage from './pages/AboutPage'
+import HomePage from './pages/HomePage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+])
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ anara-hotel/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This ensures Tailwind looks in all your source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
