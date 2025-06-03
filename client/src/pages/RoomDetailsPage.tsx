6a43efce86e9c5fd0f18174c7a04be1e802248e5
Ningendo12 <116316808+Ningendo12@users.noreply.github.com>
2025-06-03 21:10:46 +0400

    build

--- client/src/pages/RoomDetailsPage.tsx
+++ client/src/pages/RoomDetailsPage.tsx
@@ -21,6 +21,7 @@ export default function RoomDetailsPage() {
   useEffect(() => {
     const fetchRoom = async () => {
       const res = await axios.get(`https://anara-hotel.onrender.com/api/rooms`)
+      //g
       const allRooms = res.data as Room[]
       const foundRoom = allRooms.find((r) => toSlug(r.name) === slug)
       setRoom(foundRoom || null)
